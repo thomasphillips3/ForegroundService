074bd6e459cc7711f11a4039249796a43f7727ea
Thomas Phillips <tphillips@snap.com>
2018-10-02 18:43:37 -0700

    Fixed stupid typo

--- app/src/main/java/com/example/thomasphillips/foregroundservice/Constants.java
+++ app/src/main/java/com/example/thomasphillips/foregroundservice/Constants.java
@@ -2,12 +2,12 @@ package com.example.thomasphillips.foregroundservice;
 
 public class Constants {
     public interface ACTION {
-        public static string MAIN_ACTION = "com.example.thomasphillips.foregroundservice.action.main";
-        public static string PREV_ACTION = "com.example.thomasphillips.foregroundservice.action.prev";
-        public static string PLAY_ACTION = "com.example.thomasphillips.foregroundservice.action.play";
-        public static string NEXT_ACTION = "com.example.thomasphillips.foregroundservice.action.next";
-        public static string START_FOREGROUND_ACTION = "com.example.thomasphillips.foregroundservice.action.startforeground";
-        public static string STOP_FOREGROUND_ACTION = "com.example.thomasphillips.foregroundservice.action.stopforeground";
+        public static String MAIN_ACTION = "com.example.thomasphillips.foregroundservice.action.main";
+        public static String PREV_ACTION = "com.example.thomasphillips.foregroundservice.action.prev";
+        public static String PLAY_ACTION = "com.example.thomasphillips.foregroundservice.action.play";
+        public static String NEXT_ACTION = "com.example.thomasphillips.foregroundservice.action.next";
+        public static String START_FOREGROUND_ACTION = "com.example.thomasphillips.foregroundservice.action.startforeground";
+        public static String STOP_FOREGROUND_ACTION = "com.example.thomasphillips.foregroundservice.action.stopforeground";
     }
 
     public interface NOTIFICATION_ID {
